360633f280cb8b494b84ef38ef272b81215e3a4f
Laud Charles Ochei <laud.ochei@gmail.com>
2022-03-11 11:08:11 +0000

    changed allowed host in settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     #'localhost',
     #'127.0.0.1'
     #'vscode-django-tutorial.azurewebsites.net'  # Sample name only!
+    blooming-shore-74257.herokuapp.com
 ]
 
 # Application definition
