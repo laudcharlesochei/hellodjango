00f091ac181de6cef50c2538a14c72e3d6dcba19
Laud Charles Ochei <laud.ochei@gmail.com>
2022-03-11 11:55:30 +0000

    changed id of auto_field

--- hello/models.py
+++ hello/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class LogMessage(models.Model):
+    id = models.AutoField(primary_key=True)
     message = models.CharField(max_length=300)
     log_date = models.DateTimeField("date logged")
 
